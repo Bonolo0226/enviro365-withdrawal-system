a1b46f2b0cd0fb3092e03daa899031217ab3cdea
Bonolo0226 <bonololeponesa26@gmail.com>
2026-09-10 16:08:35 +0200

    Set up Spring Boot project skeleton

---
+++ bonolo/src/main/java/com/enviro/assessment/junior/bonolo/BonoloApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.bonolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonoloApplication.class, args);
+	}
+
+}
